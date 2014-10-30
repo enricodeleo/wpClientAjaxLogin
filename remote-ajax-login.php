4f8e16a176bd51115524584506d0b44cf98a48bb
Enrico Deleo <enrico@lysergicstudio.com>
2014-10-30 02:21:36 +0100

    Initial commit

---
+++ remote-ajax-login.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Remote Ajax Login
+Plugin URI: http://enricodeleo.com
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: enricodeleo
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
