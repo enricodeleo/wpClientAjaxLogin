917d3f4fbfd844338a36666dd03e6bb23498f01a
Enrico Deleo <enrico@lysergicstudio.com>
2014-10-30 02:48:30 +0100

    first version working

--- remote-ajax-login.php
+++ remote-ajax-login.php
@@ -1,11 +1,36 @@
 <?php
 
 /*
-Plugin Name: Remote Ajax Login
-Plugin URI: http://enricodeleo.com
-Description: A brief description of the Plugin.
-Version: 1.0
-Author: enricodeleo
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Plugin Name: WP Client Ajax Login
+Plugin URI: https://github.com/enricodeleo/wpClientAjaxLogin
+Description: A simple plugin that allows login to wordpress through the client's browser via AJAX.
+Version: 0.1.0
+Author: Enrico Deleo
+Author URI: http://enricodeleo.com
+License: MIT
 */
+
+function clientAjaxLogin() {
+    $user        = $_POST[ 'user' ];
+    $pwd         = $_POST[ 'pwd' ];
+    $response    = array();
+    $data = array(
+        'user_login'    => $user,
+        'user_password' => $pwd,
+        'remember'      => true //prevent the browser forgetting log in status
+    );
+
+    $signin = wp_signon( $data, false );
+
+    if ( is_wp_error( $signin ) ) {
+        $response[ 'error' ] = $signin->get_error_message();
+    } else {
+        $response[ 'success' ] = get_bloginfo( 'url' );
+    }
+
+    echo json_encode( $response );
+    exit();
+}
+
+add_action( 'wp_ajax_clientAjaxLogin', 'clientAjaxLogin' );
+add_action( 'wp_ajax_nopriv_clientAjaxLogin', 'clientAjaxLogin' );
